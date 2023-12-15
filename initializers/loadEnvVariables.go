b26947dddcab4e997a75413d6f0f29048496e52d
RafiMoreno <ahmadarrafi.m@gmail.com>
2023-12-15 21:21:17 +0700

    edit loadenvvariables for railway

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -2,13 +2,17 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); exists == false {
+		err := godotenv.Load()
+			if err != nil {
+				log.Fatal("Error loading .env file")
+			}
 	}
+	
 }
